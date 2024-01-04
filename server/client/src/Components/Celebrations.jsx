ecbec21d9b712e553fd062d33ccb092fc24ea441
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2024-01-04 23:04:37 +0530

    api commit

--- server/client/src/Components/Celebrations.jsx
+++ server/client/src/Components/Celebrations.jsx
@@ -9,7 +9,7 @@ export const Celebrations = () => {
   const [celebrationdata, setCelebrationData] = useState([]);
 
   useEffect(() => {
-    axios(`http://52.66.253.4/celebration`)
+    axios(`http://zinggalas.com/celebration`)
       .then((res) => setCelebrationData(res.data))
       .catch((err) => console.log(err));
   }, []);
