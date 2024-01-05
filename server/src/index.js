d4b2606f3ce37f78d96712e92927fd70ed1c7cea
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2024-01-06 00:55:08 +0530

    commit

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
    //aws server config
 const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build");
+const buildPath = path.join(_dirname, ".../client/build");
 
 app.use(express.static(buildPath));
 app.use(
@@ -30,7 +30,7 @@ app.use(
 
 app.get("/", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
+    path.join(__dirname, ".../client/build/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
