d41c5fafc7821d66f29ccc7a9a652065e245abaf
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2024-01-06 01:23:29 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -15,29 +15,29 @@ const app = express();
 
 app.use(express.json());
 
-   //aws server config
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build");
+//    //aws server config
+// const _dirname = path.dirname("");
+// const buildPath = path.join(_dirname, "../client/build");
 
-app.use(express.static(buildPath));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(express.static(buildPath));
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 
 
-app.get("/", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
 app.use("/celebration", celebrationController);
 app.use("/festival", festivalController);
